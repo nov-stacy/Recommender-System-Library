46bede0db74167381a196d38f88a71c52130dace
wolfchik <nov.stacy@yandex.ru>
2021-01-20 18:14:42 +0300

    Add sparse matrix to svd naive method

---
+++ recommender_system/models/factorizing_machines/svd/svd_naive.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy.sparse as sparse
+from scipy.sparse.linalg import svds
+from recommender_system.models.abstract_recommender_system import RecommenderSystem
+
+
+class SingularValueDecompositionNaiveModel(RecommenderSystem):
+    """
+    Recommender system based on singular value decomposition.
+    Naive realization: decompose the system matrix into matrices for users and items, facilitate them using
+    approximation and give ranks using the product of a row with a user and a matrix of items
+    """
+
+    def __init__(self, dimension: int) -> None:
+        """
+        :param dimension: the number of singular values to keep (if dimension is None use original decomposition)
+        """
+        self.__dimension: int = dimension
+        self.__first_matrix: np.ndarray = np.array([])
+        self.__second_matrix: np.ndarray = np.array([])
+        self.__singular_values: np.ndarray = np.array([])
+        self.__data: sparse.coo_matrix = sparse.eye(0)
+        self.__indices_ptr: np.ndarray = np.array([])
+        self.__indices: np.ndarray = np.array([])
+
+    def train(self, data: sparse.coo_matrix) -> 'SingularValueDecompositionNaiveModel':
+        csr_data: sparse.csr_matrix = data.tocsr()
+        self.__first_matrix, self.__singular_values, self.__second_matrix = svds(csr_data, k=self.__dimension)
+        self.__indices_ptr = csr_data.indptr
+        self.__indices: np.ndarray = csr_data.indices
+        self.__data = data.copy()
+        return self
+
+    def retrain(self, data: sparse.coo_matrix) -> 'SingularValueDecompositionNaiveModel':
+        return self.train(data)
+
+    def predict(self, user_index) -> np.ndarray:
+        return self.__first_matrix[user_index] @ np.diag(self.__singular_values) @ self.__second_matrix
+
