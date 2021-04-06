51fa8b18efa18995a28e0ae5dacb07d2c90ccb4d
nov-stacy <nov.stacy@yandex.ru>
2021-04-06 19:39:42 +0300

    add exceptions

--- recommender_system_library/metrics/_recall_k.py
+++ recommender_system_library/metrics/_recall_k.py
@@ -14,10 +14,21 @@ def __recall_for_one_user(true_user_pref_indices: np.ndarray,
     predicted_user_pref_indices: numpy array
         Item indices that were recommended to the user
 
+    Raises
+    ------
+    TypeError
+        If parameters don't have needed format
+    ValueError
+        If arrays don't store int non-negative values
+
     Returns
     -------
     Recall@k for user: float
     """
+
+    if type(true_user_pref_indices) != np.ndarray or type(predicted_user_pref_indices) != np.ndarray:
+        raise TypeError('Indices need to have numpy array format')
+
     if true_user_pref_indices.dtype != np.int or predicted_user_pref_indices.dtype != np.int:
         raise ValueError('Arrays should store indices')
 
@@ -44,10 +55,20 @@ def recall_k(true_preferences_indices: tp.List[np.ndarray],
     predicted_preferences_indices: array of numpy arrays
         Item indices that were recommended to the users
 
+    Raises
+    ------
+    TypeError
+        If parameters don't have needed format
+    ValueError
+        If two arrays don't have same shape
+
     Returns
     -------
     Mean of Recall@k for all users: float
     """
+
+    if type(true_preferences_indices) != list or type(predicted_preferences_indices) != list:
+        raise TypeError('Input values need to have list format')
 
     if len(true_preferences_indices) != len(predicted_preferences_indices):
         raise ValueError('Two arrays should have same shape')
