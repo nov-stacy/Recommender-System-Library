cbc8e08990f74bebbeacde7be7cb653b83b933f1
nov-stacy <nov.stacy@yandex.ru>
2021-04-13 02:35:59 +0300

    Add raising errors

--- recommender_system_library/metrics/_precision_k.py
+++ recommender_system_library/metrics/_precision_k.py
@@ -2,16 +2,18 @@ import typing as tp
 import numpy as np
 
 
-def __precision_for_one_user(true_user_pref_indices: np.ndarray,
-                             predicted_user_pref_indices: np.ndarray) -> float:
+__all__ = ['precision_k']
+
+
+def __one_user_precision(true_indices: np.ndarray, predicted_indices: np.ndarray) -> float:
     """
     Method to calculate Precision@k for one user
 
     Parameters
     ----------
-    true_user_pref_indices: numpy array
+    true_indices: numpy array
         Indices of items, about which it is known that they was liked by the user
-    predicted_user_pref_indices: numpy array
+    predicted_indices: numpy array
         Item indices that were recommended to the user
 
     Raises
@@ -26,33 +28,33 @@ def __precision_for_one_user(true_user_pref_indices: np.ndarray,
     Precision@k for user: float
     """
 
-    if type(true_user_pref_indices) != np.ndarray or type(predicted_user_pref_indices) != np.ndarray:
+    if type(true_indices) != np.ndarray or type(predicted_indices) != np.ndarray:
         raise TypeError('Indices need to have numpy array format')
 
-    if true_user_pref_indices.dtype != np.int or predicted_user_pref_indices.dtype != np.int:
-        raise ValueError('Arrays should store indices')
+    if true_indices.shape[0] and true_indices.dtype != np.int or \
+            predicted_indices.shape[0] and predicted_indices.dtype != np.int:
+        raise TypeError('Arrays should store indices')
 
-    if np.count_nonzero(true_user_pref_indices < 0) != 0 or np.count_nonzero(predicted_user_pref_indices < 0) != 0:
+    if np.count_nonzero(true_indices < 0) != 0 or np.count_nonzero(predicted_indices < 0) != 0:
         raise ValueError('Arrays should store non-negative indices')
 
-    if predicted_user_pref_indices.shape[0] == 0:
+    if predicted_indices.shape[0] == 0:
         return 0
 
-    true_predicted_pref_number: float = len(set(predicted_user_pref_indices).intersection(true_user_pref_indices))
-    return true_predicted_pref_number / predicted_user_pref_indices.shape[0]
+    true_predicted_pref_number: float = len(set(predicted_indices).intersection(true_indices))
+    return true_predicted_pref_number / predicted_indices.shape[0]
 
 
-def precision_k(true_preferences_indices: tp.List[np.ndarray],
-                predicted_preferences_indices: tp.List[np.ndarray]) -> float:
+def precision_k(true_indices: tp.List[np.ndarray], predicted_indices: tp.List[np.ndarray]) -> float:
     """
     Method to calculate Precision@k for all users
     (Precision at k is the proportion of recommended items in the top-k set that are relevant)
 
     Parameters
     ----------
-    true_preferences_indices: array of numpy arrays
+    true_indices: array of numpy arrays
         Indices of items, about which it is known that they was liked by the users
-    predicted_preferences_indices: array of numpy arrays
+    predicted_indices: array of numpy arrays
         Item indices that were recommended to the users
 
     Raises
@@ -67,12 +69,14 @@ def precision_k(true_preferences_indices: tp.List[np.ndarray],
     Mean of Precision@k for all users: float
     """
 
-    if type(true_preferences_indices) != list or type(predicted_preferences_indices) != list:
+    def one_user_precision(index) -> float:
+        return __one_user_precision(true_indices[index], predicted_indices[index])
+
+    if type(true_indices) != list or type(predicted_indices) != list:
         raise TypeError('Input values need to have list format')
 
-    if len(true_preferences_indices) != len(predicted_preferences_indices):
+    if len(true_indices) != len(predicted_indices):
         raise ValueError('Two arrays should have same shape')
 
-    indices = np.arange(len(true_preferences_indices))
-    return np.vectorize(lambda index: __precision_for_one_user(true_preferences_indices[index],
-                                                               predicted_preferences_indices[index]))(indices).mean()
+    indices = np.arange(len(true_indices))
+    return np.vectorize(one_user_precision)(indices).mean()
--- recommender_system_library/metrics/_recall_k.py
+++ recommender_system_library/metrics/_recall_k.py
@@ -2,16 +2,18 @@ import typing as tp
 import numpy as np
 
 
-def __recall_for_one_user(true_user_pref_indices: np.ndarray,
-                          predicted_user_pref_indices: np.ndarray) -> float:
+__all__ = ['recall_k']
+
+
+def __one_user_recall(true_indices: np.ndarray, predicted_indices: np.ndarray) -> float:
     """
     Method to calculate Recall@k for one user
 
     Parameters
     ----------
-    true_user_pref_indices: numpy array
+    true_indices: numpy array
         Indices of items, about which it is known that they was liked by the user
-    predicted_user_pref_indices: numpy array
+    predicted_indices: numpy array
         Item indices that were recommended to the user
 
     Raises
@@ -19,40 +21,40 @@ def __recall_for_one_user(true_user_pref_indices: np.ndarray,
     TypeError
         If parameters don't have needed format
     ValueError
-        If arrays don't store int non-negative values
+        If arrays don't store non-negative values
 
     Returns
     -------
     Recall@k for user: float
     """
 
-    if type(true_user_pref_indices) != np.ndarray or type(predicted_user_pref_indices) != np.ndarray:
+    if type(true_indices) != np.ndarray or type(predicted_indices) != np.ndarray:
         raise TypeError('Indices need to have numpy array format')
 
-    if true_user_pref_indices.dtype != np.int or predicted_user_pref_indices.dtype != np.int:
-        raise ValueError('Arrays should store indices')
+    if true_indices.shape[0] and true_indices.dtype != np.int or \
+            predicted_indices.shape[0] and predicted_indices.dtype != np.int:
+        raise TypeError('Arrays should store indices')
 
-    if np.count_nonzero(true_user_pref_indices < 0) != 0 or np.count_nonzero(predicted_user_pref_indices < 0) != 0:
+    if np.count_nonzero(true_indices < 0) != 0 or np.count_nonzero(predicted_indices < 0) != 0:
         raise ValueError('Arrays should store non-negative indices')
 
-    if true_user_pref_indices.shape[0] == 0:
+    if true_indices.shape[0] == 0:
         return 0
 
-    true_predicted_pref_number: float = len(set(predicted_user_pref_indices).intersection(true_user_pref_indices))
-    return true_predicted_pref_number / true_user_pref_indices.shape[0]
+    true_predicted_pref_number: float = len(set(predicted_indices).intersection(true_indices))
+    return true_predicted_pref_number / true_indices.shape[0]
 
 
-def recall_k(true_preferences_indices: tp.List[np.ndarray],
-             predicted_preferences_indices: tp.List[np.ndarray]) -> float:
+def recall_k(true_indices: tp.List[np.ndarray], predicted_indices: tp.List[np.ndarray]) -> float:
     """
     Method to calculate Recall@k for all users
     (Recall at k is the proportion of relevant items found in the top-k recommendations)
 
     Parameters
     ----------
-    true_preferences_indices: array of numpy arrays
+    true_indices: array of numpy arrays
         Indices of items, about which it is known that they was liked by the users
-    predicted_preferences_indices: array of numpy arrays
+    predicted_indices: array of numpy arrays
         Item indices that were recommended to the users
 
     Raises
@@ -67,12 +69,14 @@ def recall_k(true_preferences_indices: tp.List[np.ndarray],
     Mean of Recall@k for all users: float
     """
 
-    if type(true_preferences_indices) != list or type(predicted_preferences_indices) != list:
+    def one_user_recall(index) -> float:
+        return __one_user_recall(true_indices[index], predicted_indices[index])
+
+    if type(true_indices) != list or type(predicted_indices) != list:
         raise TypeError('Input values need to have list format')
 
-    if len(true_preferences_indices) != len(predicted_preferences_indices):
+    if len(true_indices) != len(predicted_indices):
         raise ValueError('Two arrays should have same shape')
 
-    indices = np.arange(len(true_preferences_indices))
-    return np.vectorize(lambda index: __recall_for_one_user(true_preferences_indices[index],
-                                                            predicted_preferences_indices[index]))(indices).mean()
+    indices = np.arange(len(true_indices))
+    return np.vectorize(one_user_recall)(indices).mean()
